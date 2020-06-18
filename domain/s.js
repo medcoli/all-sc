d160e1fd605bc0f4515915eec56462243177d1b2
M3d <43959128+medcoli@users.noreply.github.com>
2020-06-18 14:18:04 +0700

    Create s.js

---
+++ domain/s.js
@@ -0,0 +1,18 @@
+document.getElementById("tai").innerHTML = "<b>
+    <h1 style="text-align: center;">List Domainnya</h1><br>
+<center><span style="color: green;"><br>
+<li>hotts819.ml</li>
+<li>hotts820.ga</li>
+<li>hotts821.ga</li>
+<li>bokep221.ml</li>
+<li>bokep222.tk</li>
+<li>bokep223.ga</li>
+<li>kosong</li>
+<li>kosong</li>
+<li>kosong</li>
+<li>-</li>
+<li>-</li>
+<li>-</li>
+<li>-</li>
+<li>-</li>
+<li>-</li></span>";
