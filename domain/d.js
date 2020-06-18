1bafe267e61af6fa4259f2dc033029d40492c42f
M3d <43959128+medcoli@users.noreply.github.com>
2020-06-18 14:24:14 +0700

    Create d.js

---
+++ domain/d.js
@@ -0,0 +1,1 @@
+document.getElementById("isi").innerHTML = "<div class='kontol'>script rusak<p>download ulang<p><a href='http://www.mediafire.com/file/631ry10pk6n18h9/grupwajoss.zip'>KLIKKKK DISINI</a><p></div>";
